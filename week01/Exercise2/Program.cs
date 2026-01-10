7af762e16e5e143da9d7bd1de73b8baf3aef6d43
Benjamin Sterling Silver <bsilver47@outlook.com>
2026-01-10 15:06:42 -0700

    Implement grade percentage evaluation and pass/fail messaging

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is the grade percentage?");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string letterGrade = "";
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Your letter grade is A.");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("Your letter grade is B.");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Your letter grade is C.");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Your letter grade is D.");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F.");
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you have not passed the course. Better luck next time!");
+        }
     }
 }
+
+// {
+//     Console.WriteLine("What is your first name?");
+//     string firstName = Console.ReadLine();
+//     Console.WriteLine("What is your last name?");
+//     string lastName = Console.ReadLine();
+//     Console.WriteLine($"That means you are {lastName}, {firstName} {lastName}");
+// }
+// }
