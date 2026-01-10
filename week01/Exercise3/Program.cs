86d1a53d482316ab15049d9250afc61d06de029d
Benjamin Sterling Silver <bsilver47@outlook.com>
2026-01-10 15:35:48 -0700

    Implement number guessing game with replay functionality

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+        while (playAgain)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 101);
+            Console.WriteLine("What is your guess?");
+            int guess = int.Parse(Console.ReadLine());
+            int attempts = 1;
+            while (guess != number)
+            {
+                if (guess < number)
+                {
+                    Console.WriteLine("Too low. Guess again:");
+                }
+                else
+                {
+                    Console.WriteLine("Too high. Guess again:");
+                }
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+            }
+            Console.WriteLine("Congratulations! You guessed the number!");
+            Console.WriteLine($"It took you {attempts} attempts to guess the number.");
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            string response = Console.ReadLine().ToLower();
+            if (response != "yes")
+            {
+                playAgain = false;
+            }
+        }
     }
 }
