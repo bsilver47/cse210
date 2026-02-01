3286432470fd2a2aa053c0b3253be9c65b05dfbb
Benjamin Sterling Silver <bsilver47@outlook.com>
2026-01-31 17:08:46 -0700

    Add Word Class

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace ScriptureMemorizer
+{
+    class Word
+    {
+        private string _text;
+        private bool _isHidden;
+
+        public Word(string text)
+        {
+            _text = text;
+            _isHidden = false;
+        }
+
+        public void Hide()
+        {
+            _isHidden = true;
+        }
+
+        public bool IsHidden()
+        {
+            return _isHidden;
+        }
+
+        public string GetDisplayText()
+        {
+            return _isHidden ? new string('_', _text.Length) : _text; // Keep an eye on this section and test functionality
+        }
+    }
+}
